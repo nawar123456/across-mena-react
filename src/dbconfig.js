05958def6be01b53ad66bfdd4e4068bc351ce811
nawarHasan <nawargitlab@gmail.com>
2025-09-25 16:32:12 +0300

    DB

--- src/dbconfig.js
+++ src/dbconfig.js
@@ -1,7 +1,7 @@
 // This file for choose what the DB connect in this project :
-const API_BASE_URL = "http://localhost:8000";
+// const API_BASE_URL = "http://localhost:8000";
 
-//  const API_BASE_URL = "https://across-mena.com";
+ const API_BASE_URL = "https://across-mena.com";
 //  const API_BASE_URL = "http://148.230.71.233:8000";
 //  const API_BASE_URL = "https://acrossmena.xyz/";
 
